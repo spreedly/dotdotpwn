0a578d84290f3e80ae530d4fececabb0b1a38c35
Adam Williams <adam@thewilliams.ws>
2013-10-11 09:46:43 -0400

    Parameterize script, perl versioned.

--- pwn.sh
+++ pwn.sh
@@ -1,1 +1,5 @@
-carton exec -- perl dotdotpwn.pl -m http -h <IPADDRESS> -o unix -d 1
+# Allow for attacking without any hostname.
+# carton exec -- perl dotdotpwn.pl -m http -h $1 -S -o unix -d 1
+
+# Allow for attacking with hostname, over https.
+carton exec -- perl dotdotpwn.pl -m http -S -h $1 -o unix -d 1
