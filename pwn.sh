e2eaad96f8cef46971963ffaeb7bc8d40894cba7
Adam Williams <adam@thewilliams.ws>
2015-11-12 17:13:12 -0500

    We can handle a faster run

--- pwn.sh
+++ pwn.sh
@@ -2,4 +2,4 @@
 # carton exec -- perl dotdotpwn.pl -m http -h $1 -S -o unix -d 1
 
 # Allow for attacking with hostname, over https.
-carton exec -- perl dotdotpwn.pl -m http -C -S -h $1 -o unix -d 1
+carton exec -- perl dotdotpwn.pl -m http -C -S -h $1 -o unix -d 1 -t 50
